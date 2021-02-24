5663aab031cbbbd92d554d5f1530dc80f80a18ba
Julie Mahe <j-mahe@hotmail.fr>
2021-02-24 23:29:47 +0100

    finish carroussel

--- containers/RoomScreen.js
+++ containers/RoomScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Dimensions } from "react";
 import { Text, View, Image, StyleSheet, Button } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 const axios = require("axios");
@@ -7,6 +7,8 @@ const RoomScreen = ({ route, navigation }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const { id } = route.params;
+  // const window = Dimensions.get("window");
+  // const screen = Dimensions.get("screen");
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
@@ -18,15 +20,59 @@ const RoomScreen = ({ route, navigation }) => {
 
     fetchData();
   }, []);
-  console.log(route.params.id);
+  // console.log(data.photos);
   return !loading ? (
     <Text>En cours de chargement</Text>
   ) : (
     <ScrollView>
-      <Text>l'id : {JSON.stringify(id)}</Text>
+      <ScrollView horizontal>
+        <View style={{ flexDirection: "row" }}>
+          {data.photos.map((elem) => {
+            // console.log(elem.url);
+            return (
+              <Image
+                key={elem.picture_id}
+                source={{ uri: elem.url }}
+                style={style.photos}
+                resizeMode="cover"
+              ></Image>
+            );
+          })}
+        </View>
+      </ScrollView>
+      <Text style={style.price}>{data.price} €</Text>
+
+      <View style={{ width: "95%" }}>
+        <Text style={style.title}>{data.title}</Text>
+      </View>
+      {/* <Text>l'id : {JSON.stringify(id)}</Text> */}
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </ScrollView>
   );
 };
 
 export default RoomScreen;
+
+const style = StyleSheet.create({
+  photos: {
+    height: 250,
+    width: 400,
+    // width: window,
+    marginRight: 10,
+  },
+  price: {
+    backgroundColor: "#2A2A2A",
+    color: "white",
+    width: "28%",
+    fontSize: 21,
+    // padding: 10,
+    textAlign: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    position: "relative",
+    bottom: 80,
+  },
+  title: {
+    fontSize: 24,
+  },
+});
